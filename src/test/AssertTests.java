9b3aaa6dca09f02cb7e7ca9c5ba94d7c792251ac
Helga Zsikai-Najzer <helga.zsikainajzer@logmein.com>
2019-03-29 08:45:53 +0100

    Update assert tests

--- src/test/AssertTests.java
+++ src/test/AssertTests.java
@@ -1,17 +1,3 @@
-/*import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.CoreMatchers.both;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.everyItem;
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
-import static org.hamcrest.CoreMatchers.startsWith;
-import org.hamcrest.core.CombinableMatcher;
-
-
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -55,34 +41,8 @@ public class AssertTests {
         assertSame(aNumber, aNumber, "should be same");
     }
 
-    // JUnit Matchers assertThat
-    @Test
-    public void testAssertThatBothContainsString() {
-        assertThat("albumen", both(containsString("a")).and(containsString("b")));
-    }
-
-    @Test
-    public void testAssertThatHasItems() {
-        assertThat(Arrays.asList("one", "two", "three"), hasItems("one", "three"));
-    }
-
-    @Test
-    public void testAssertThatEveryItemContainsString() {
-        assertThat(Arrays.asList(new String[] { "fun", "ban", "net" }), everyItem(containsString("n")));
-    }
-
-    // Core Hamcrest Matchers with assertThat
-    @Test
-    public void testAssertThatHamcrestCoreMatchers() {
-        assertThat("good", allOf(equalTo("good"), startsWith("good")));
-        assertThat("good", not(allOf(equalTo("bad"), equalTo("good"))));
-        assertThat("good", anyOf(equalTo("bad"), equalTo("good")));
-        assertThat(new Object(), not(sameInstance(new Object())));
-    }
-
     @Test
     public void testAssertTrue() {
-        assertTrue("failure - should be true", true);
+        assertTrue(true, "failure - should be true");
     }
 }
-*/
